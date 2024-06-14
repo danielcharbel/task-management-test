f23a925f569d99335e6411a72cce67953d4f514c
Daniel Charbel <daniel.charbel@capital-banking.com>
2024-06-14 22:49:12 +0300

    Task Management Test Project

---
+++ src/main/java/com/danielcharbel/taskmanagement/HelloController.java
@@ -0,0 +1,13 @@
+package com.danielcharbel.taskmanagement;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
